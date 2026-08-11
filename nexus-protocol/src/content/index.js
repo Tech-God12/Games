139e7490bb6b668e2b7e86fc439415f9254067a3
Tech-God12 <206826175+Tech-God12@users.noreply.github.com>
2026-08-11 00:38:32 +0000

    Add precision2 weapons (marksman, crossbow, mythic Galileo)
    Co-authored-by: arena-agent <297053741+arena-agent@users.noreply.github.com>

--- nexus-protocol/src/content/index.js
+++ nexus-protocol/src/content/index.js
@@ -26,6 +26,7 @@ import '../weapons/definitions/energy2.js';
 import '../weapons/definitions/melee2.js';
 import '../weapons/definitions/special2.js';
 import '../weapons/definitions/heavy4.js';
+import '../weapons/definitions/precision2.js';
 
 // Enemies
 import '../enemies/definitions/basic.js';
