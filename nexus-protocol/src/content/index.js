60bba00af7ce0fd3b436747d570cc0d172712a65
Tech-God12 <206826175+Tech-God12@users.noreply.github.com>
2026-08-11 00:40:58 +0000

    Add sidearms2 (18 more pistols, void/inferno legendaries)
    Co-authored-by: arena-agent <297053741+arena-agent@users.noreply.github.com>

--- nexus-protocol/src/content/index.js
+++ nexus-protocol/src/content/index.js
@@ -27,6 +27,7 @@ import '../weapons/definitions/melee2.js';
 import '../weapons/definitions/special2.js';
 import '../weapons/definitions/heavy4.js';
 import '../weapons/definitions/precision2.js';
+import '../weapons/definitions/sidearms2.js';
 
 // Enemies
 import '../enemies/definitions/basic.js';
